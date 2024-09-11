59fb29bffd5d15dd0af1e3ec0fba128d592c46f5
Sam Maton <sam.maton91@gmail.com>
2024-09-11 21:09:00 +0100

    Added player to the screen

--- main.py
+++ main.py
@@ -1,17 +1,27 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
-  pygame.init()
-  screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
 
-  while True:
-    for event in pygame.event.get():
-      if event.type == pygame.QUIT:
-        return
-      
-    screen.fill('black')
-    pygame.display.flip()
+    player1 = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+    
+        screen.fill('black')
+        player1.draw(screen)
+        
+        
+        pygame.display.flip()
+        # Limit framerate to 60fps
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
-  main()
+    main()
