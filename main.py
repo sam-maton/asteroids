5e87974bb44b3ba9cfeb8a0e3b7416a63578d039
Sam Maton <sam.maton91@gmail.com>
2024-09-12 15:01:52 +0100

    added shooting functionality

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from constants import *
 from player import Player
 from asteroid import Asteroid
 from asteroid_field import AsteroidField
+from shot import Shot
 
 def main():
     pygame.init()
@@ -14,10 +15,12 @@ def main():
     updateable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
 
     Player.containers = (updateable, drawable)
     Asteroid.containers = (asteroids, updateable, drawable)
     AsteroidField.containers = (updateable)
+    Shot.containers = (shots, updateable, drawable)
 
     player1 = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     field = AsteroidField()
--- player.py
+++ player.py
@@ -1,5 +1,6 @@
 import pygame
 from circleshape import CircleShape
+from shot import Shot
 from constants import *
 
 class Player(CircleShape):
@@ -43,3 +44,12 @@ class Player(CircleShape):
         
         if keys[pygame.K_s] or keys[pygame.K_DOWN]:
             self.move(-dt)
+        
+        if keys[pygame.K_SPACE]:
+            self.shoot()
+    
+    def shoot(self):
+        shot = Shot(self.position.x, self.position.y, SHOT_RADIUS)
+        forward = pygame.Vector2(0,1).rotate(self.rotation)
+
+        shot.velocity = forward * PLAYER_SHOT_SPEED
